dd624d6c0ea78bcde33d8029510f002073329327
Anaswara6238 <152838025+Anaswara6238@users.noreply.github.com>
2023-12-06 20:17:53 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById("pic").src="https://th.bing.com/th/id/OIP.prE2JXqapG1eB-R6J_ZBIgHaHk?rs=1&pid=ImgDetMain"
+}
+function off(){
+    document.getElementById("pic").src="https://toppng.com/public/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png"
+
+}
